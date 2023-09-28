6f6a0722852aa1622a596f0125c877720064c8b4
TarasLukasuk <121807502+TarasLukasuk@users.noreply.github.com>
2023-09-28 23:15:08 +0300

    Добавьте файлы проекта.

---
+++ Сomputer shutdown timer/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using Сomputer_shutdown_timer.Pages;
+
+namespace Сomputer_shutdown_timer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Focus();
+        }
+
+        private int received_Hours = 0, received_Minutes = 0, received_Seconds = 0;
+
+        private void Toolbar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
+
+        async void Async_blinking_animation()
+        {
+            while (true)
+            {
+                Right_blink_system_time.Visibility = Visibility.Hidden;
+                Left_blink_system_time.Visibility = Visibility.Hidden;
+                await Task.Delay(500);
+
+                Right_blink_system_time.Visibility = Visibility.Visible;
+                Left_blink_system_time.Visibility = Visibility.Visible;
+                await Task.Delay(500);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) => Async_blinking_animation();
+
+        private void start_countdown_Click(object sender, RoutedEventArgs e)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\CST");
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(Setting_time);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+
+            if (key != null)
+            {
+                this.Visibility = Visibility.Hidden;
+            }
+
+            Tray.Visibility = Visibility.Visible;
+
+            Get_setting(ref received_Hours, ref received_Minutes, ref received_Seconds);
+            Display_time_settings.Text = $"{received_Hours}:{received_Minutes}:{received_Seconds}";
+        }
+
+        private void Setting_time(object? sender, EventArgs e)
+        {
+            Show_system_time();
+            Time_comparison();
+        }
+
+        private void Show_system_time()
+        {
+            int hours_System_Time = DateTime.Now.Hour,
+                minute_System_Time = DateTime.Now.Minute,
+                seconds_System_Time = DateTime.Now.Second;
+
+            if (seconds_System_Time < 10)
+            {
+                Seconds_system_time.Text = "0" + seconds_System_Time;
+            }
+            else
+            {
+                Seconds_system_time.Text = seconds_System_Time.ToString();
+            }
+
+            if (minute_System_Time < 10)
+            {
+                Minute_system_time.Text = "0" + minute_System_Time;
+            }
+            else
+            {
+                Minute_system_time.Text = minute_System_Time.ToString();
+            }
+
+            if (hours_System_Time < 10)
+            {
+                Hours_system_time.Text = "0" + hours_System_Time;
+            }
+            else
+            {
+                Hours_system_time.Text = hours_System_Time.ToString();
+            }
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e) => Show_pages.Content = new settings();
+
+        private void Get_setting(ref int hours, ref int minute, ref int second)
+        {
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\CST"))
+                {
+                    hours = int.Parse(key.GetValue("Hours", hours).ToString());
+                    minute = int.Parse(key.GetValue("Minute", minute).ToString());
+                    second = int.Parse(key.GetValue("Second", second).ToString());
+                }
+            }
+            catch (Exception)
+            {
+
+                Show_pages.Content = new settings();
+            }
+        }
+
+        private void Tray_TrayLeftMouseDown(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Visible;
+            Tray.Visibility = Visibility.Hidden;
+        }
+
+        private void Tray_setting_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Visible;
+            Show_pages.Content = new settings();
+        }
+
+        private void Tray_close_Click(object sender, RoutedEventArgs e) => this.Close();
+
+        private void Exit_program_Click(object sender, RoutedEventArgs e) => this.Close();
+
+        private void Time_comparison()
+        {
+            if (DateTime.Now.Hour == received_Hours && DateTime.Now.Minute == received_Minutes && DateTime.Now.Second == received_Seconds)
+            {
+                Process.Start("shutdown", "/s /t 0");
+            }
+        }
+
+        private void Roll_into_tray_Click(object sender, RoutedEventArgs e)
+        {
+            Tray_visibility();
+        }
+
+        private void Tray_visibility()
+        {
+            this.Visibility = Visibility.Hidden;
+            Tray.Visibility = Visibility.Visible;
+        }
+
+    }
+}
---
+++ Сomputer shutdown timer/Pages/settings.xaml.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Сomputer_shutdown_timer.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для settings.xaml
+    /// </summary>
+    public partial class settings : Page
+    {
+        public settings()
+        {
+            InitializeComponent();
+        }
+
+        private const int MAX_VALUE_HOURS = 23;
+        private const int MIN_VALUE_HOURS = 0;
+        private const int MAX_VALUE_SECOND_MINUTE = 59;
+        private const int MIN_VALUE_SECOND_MINUTE = 0;
+
+        private int hours = 0, minute = 0, second = 0;
+
+        private void Reducing_number_hours_Click(object sender, RoutedEventArgs e)
+        {
+            if (hours > MIN_VALUE_HOURS)
+            {
+                hours--;
+                Content_hours.Text = hours.ToString();
+            }
+
+            if (hours < 10)
+            {
+                Content_hours.Text = "0" + hours.ToString();
+            }
+
+            Shutdown_time();
+        }
+
+        private void Increasing_number_hours_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (hours < MAX_VALUE_HOURS)
+            {
+                hours++;
+                Content_hours.Text = hours.ToString();
+            }
+
+            if (hours < 10)
+            {
+                Content_hours.Text = "0" + hours.ToString();
+            }
+
+            Shutdown_time();
+        }
+
+        private void Reducing_number_minute_Click(object sender, RoutedEventArgs e)
+        {
+            if (minute > MIN_VALUE_SECOND_MINUTE)
+            {
+                minute--;
+                Content_minute.Text = minute.ToString();
+            }
+
+            if (minute < 10)
+            {
+                Content_minute.Text = "0" + minute.ToString();
+            }
+
+            Shutdown_time();
+        }
+
+        private void Increasing_number_minute_Click(object sender, RoutedEventArgs e)
+        {
+            if (minute < MAX_VALUE_SECOND_MINUTE)
+            {
+                minute++;
+                Content_minute.Text = minute.ToString();
+            }
+
+            if (minute < 10)
+            {
+                Content_minute.Text = "0" + minute.ToString();
+            }
+
+            Shutdown_time();
+        }
+
+        private void Increasing_number_second_Click(object sender, RoutedEventArgs e)
+        {
+            if (second < MAX_VALUE_SECOND_MINUTE)
+            {
+                second++;
+                Content_second.Text = second.ToString();
+            }
+
+            if (second < 10)
+            {
+                Content_second.Text = "0" + second.ToString();
+            }
+
+            Shutdown_time();
+        }
+
+        private void Return_main_page_Click(object sender, RoutedEventArgs e)=> this.Visibility = Visibility.Hidden;
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            Save_settings();
+            Closing_program();
+        }
+
+        private void Reducing_number_second_Click(object sender, RoutedEventArgs e)
+        {
+            if (second > MIN_VALUE_SECOND_MINUTE)
+            {
+                second--;
+                Content_second.Text = second.ToString();
+            }
+
+            if (second < 10)
+            {
+                Content_second.Text = "0" + second.ToString();
+            }
+
+            Shutdown_time();
+        }
+
+        private void Shutdown_time() => Shutdown_time_text.Text = $"Ваш комп'ютер вимкнеця о {hours}:{minute}:{second}";
+
+        private void Save_settings()
+        {
+            using (RegistryKey key=Registry.CurrentUser.CreateSubKey(@"Software\CST"))
+            {
+                key.SetValue("Hours",hours);
+                key.SetValue("Minute", minute);
+                key.SetValue("Second", second);
+            }
+        }
+
+        private void Closing_program()=>Process.GetCurrentProcess().Kill();
+    }
+}
